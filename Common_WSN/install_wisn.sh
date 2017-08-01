e3546579266ad28428c8bb0a8b1c43b3fcdb25e0
danrustia11 <dan_rustia@hotmail.com>
2017-08-01 19:25:43 +0800

    Create install_wisn.sh

---
+++ Common_WSN/install_wisn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer -y
+
+git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
+cd RPi_Cam_Web_Interface
+chmod u+x *.sh
+./install.sh
+cd
+
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+sudo apt-get install build-essential python-dev -y
+cd Adafruit_Python_DHT
+sudo python setup.py install
+cd
+
+
+

100b257f313b2eaa8e3a9054792eb8e9a83749c1
danrustia11 <dan_rustia@hotmail.com>
2017-08-16 17:31:11 +0800

    Update install_wisn.sh

--- Common_WSN/install_wisn.sh
+++ Common_WSN/install_wisn.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo rpi-update -y
-sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer -y
 
-cd /home/pi
-git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
-cd RPi_Cam_Web_Interface
-chmod u+x *.sh
-./install.sh
-cd /home/pi
+read -p "Install WiSN? Y/N  " -n 1 -r
+echo
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo rpi-update -y
+    sudo apt-get install realvnc-vnc-server realvnc-vnc-viewer -y
+    
+    cd /home/pi
+    git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
+    cd RPi_Cam_Web_Interface
+    chmod u+x *.sh
+    ./install.sh
+    cd /home/pi
+    
+    git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+    sudo apt-get install build-essential python-dev -y
+    cd Adafruit_Python_DHT
+    sudo python setup.py install
+    cd /home/pi
+    mkdir PEST_IMAGES
+fi
 
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-sudo apt-get install build-essential python-dev -y
-cd Adafruit_Python_DHT
-sudo python setup.py install
-cd /home/pi
 
-mkdir PEST_IMAGES
+
+
+echo "Please enter node number:"
+read input_variable
+echo "New node number: $input_variable"
+
+cat > NODE_NUM.txt <<- EOF
+    $input_variable
+EOF
+
+echo "Please enter DB number:"
+read input_variable
+echo "New DB number: $input_variable"
+
+cat > DB_NUM.txt <<- EOF
+    $input_variable
+EOF
+
+
 
 

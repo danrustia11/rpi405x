9ddf741b4220766b7a7d931d7102e982e52b4adf
danrustia11 <dan_rustia@hotmail.com>
2017-08-16 17:40:06 +0800

    Update edit_startup.sh

--- Common_WSN/edit_startup.sh
+++ Common_WSN/edit_startup.sh
@@ -4,9 +4,8 @@ echo "Autorun which WSN/WiSN program? i.e. CMN_WiSN_USB.py"
 read input_variable
 
 cat > run_wisn.sh <<- EOF
-    #!/bin/bash
-        #!/bin/bash
-    python /home/pi/rpi405x/Common_WSN/$input_variable
+#!/bin/bash
+python /home/pi/rpi405x/Common_WSN/$input_variable
 EOF
 
 sudo cp /home/pi/rpi405x/Common_WSN/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
